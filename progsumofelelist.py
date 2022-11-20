e073e9a7b69ad422c941f2819adf59d2d00bb01e
Rushikesh Badgujar <82447941+rishi7822@users.noreply.github.com>
2022-11-21 01:17:53 +0530

    Add files via upload

---
+++ progsumofelelist.py
@@ -0,0 +1,15 @@
+list = [34,56,77,88,32,11]
+
+#sum() 
+total = sum(list)
+
+print("sum of all elements in list is:",total)
+
+#another way
+totals = 0
+
+list1 = [24,56,77,453,423,2,3]
+for i in range(0,len(list1)):
+    totals = totals + list1[i]
+
+print("sum of all i in the lists is :",totals)
